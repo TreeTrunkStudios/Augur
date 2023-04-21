baa457a990efdaa7940d8268866b245140bbf98a
sidneyjtowers@gmail.com <sidneyjtowers@gmail.com>
2023-04-21 17:37:50 -0500

    Initial Creation
    Initial creation of the game (currently a blank project).

---
+++ Source/CornersOfTheEarth/CornersOfTheEarthGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CornersOfTheEarthGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CORNERSOFTHEEARTH_API ACornersOfTheEarthGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
